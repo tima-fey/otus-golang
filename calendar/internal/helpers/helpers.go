432e28f45e13dfc09be4d621e8f3e4b7b18e942f
Timofey <timonnius@gmail.com>
2020-02-22 23:56:13 +0300

    add config and server

---
+++ calendar/internal/helpers/helpers.go
@@ -0,0 +1,55 @@
+package helpers
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Address  string `json:"address"`
+	Port     int    `json:"port"`
+	LogFile  string `json:"log_file"`
+	LogLevel string `json:"log_level"`
+}
+
+func checkLogLvl(logLvl string) {
+	switch logLvl {
+	case
+		"info",
+		"warn",
+		"debug",
+		"error":
+		return
+	}
+	log.Fatal("Error: plese specify correct log lvl (info, warn, debug, error)")
+	return
+}
+func checkPort(port int) {
+	if port > 65536 {
+		log.Fatal("Error port mast be less than 65536")
+	}
+	if port < 1 {
+		log.Fatal("Error port mast be greater than 1")
+	}
+	return
+}
+
+func GetConfig() Config {
+	var config string
+	flag.StringVar(&config, "config", "", "osource file")
+	flag.Parse()
+	file, err := ioutil.ReadFile(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := Config{}
+	err = json.Unmarshal([]byte(file), &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	checkLogLvl(data.LogLevel)
+	checkPort(data.Port)
+	return data
+}
